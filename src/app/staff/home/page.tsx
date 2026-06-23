89498b5a72cf65e971c45613bd3f8208e5f5969a
amixermmp <amixerversatile@gmail.com>
2026-06-23 23:56:24 +0700

    fix ts types

--- src/app/staff/home/page.tsx
+++ src/app/staff/home/page.tsx
@@ -59,7 +59,8 @@ export default async function StaffHomePage() {
   ])
 
   const staffName = staffRow?.name ?? 'Staff'
-  const branchName = (staffRow?.branches as { name: string } | null)?.name ?? 'Kuma Bikes'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const branchName = (staffRow as any)?.branches?.name ?? 'Kuma Bikes'
   const allJobs: Rental[] = [
     ...((overdue ?? []) as Rental[]),
     ...((dueSoon ?? []) as Rental[]),
