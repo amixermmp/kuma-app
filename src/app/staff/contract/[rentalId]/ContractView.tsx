79760359cb8866c21cc6e2ff9ca9558775e16145
amixermmp <amixerversatile@gmail.com>
2026-09-11 17:09:29 +0700

    style: enlarge branch logo on rental contract header

--- src/app/staff/contract/[rentalId]/ContractView.tsx
+++ src/app/staff/contract/[rentalId]/ContractView.tsx
@@ -93,7 +93,7 @@ export default function ContractView({ rental, shop, branchName }: { rental: any
           <div style={{ textAlign: 'center', borderBottom: '1.5px solid #111', paddingBottom: '10px', marginBottom: '12px' }}>
             {shop.logo_url && (
               // eslint-disable-next-line @next/next/no-img-element
-              <img src={shop.logo_url} alt={shopName} style={{ width: '56px', height: '56px', objectFit: 'contain', margin: '0 auto 8px' }} />
+              <img src={shop.logo_url} alt={shopName} style={{ width: '110px', height: '110px', objectFit: 'contain', margin: '0 auto 8px' }} />
             )}
             <div style={{ fontSize: '15px', fontWeight: 700 }}>สัญญาเช่ารถมอเตอร์ไซค์ / HIRING AGREEMENT MOTOR BIKE</div>
             <div style={{ fontSize: '12px', color: '#555', marginTop: '2px' }}>{shopLine}</div>
